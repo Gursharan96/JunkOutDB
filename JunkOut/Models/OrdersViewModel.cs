a88449b0e26780a5862880556af1382cbfe45b83
Gursharan Deol <gurdeol02@hotmail.com>
2017-11-17 19:41:32 -0500

    Added Method for Index, Details and Delete

---
+++ JunkOut/Models/OrdersViewModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using JunkOutDBModel;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+namespace JunkOut.Models
+{
+    public class OrdersViewModel
+    {
+        
+        public Order order { get; set; }
+
+        public Customer customer { get; set; }
+        public Address address { get; set; }
+
+        public Bin bin { get; set; }
+
+        public TransferStation transferStation { get; set; }
+
+
+       
+     //   public List<SelBin> SelBins { get; set; }
+
+     //   public List<SelTranferStation> SelTranferStations { get; set; }
+
+
+
+/*
+        public OrdersViewModel()
+        {
+            SelBins = new List<SelBin>();
+            SelTranferStations = new List<SelTranferStation>();
+
+        }
+
+    */
+    }
+
+    /*
+    public class SelBin
+    {
+
+        public int Id { get; set; }
+
+        public bool isBinSelected { get; set; }
+
+        public string Name { get; set; }
+    }
+
+    public class SelTranferStation
+    {
+
+        public int Id { get; set; }
+
+        public bool isTranferStationSelected { get; set; }
+
+        public string Address { get; set; }
+    }
+    */
+
+}
