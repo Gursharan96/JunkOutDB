0932d8c98d6a6247448b63d4c30765518e609793
Ramsha Tariq <rmstq@outlook.com>
2017-11-25 11:09:46 -0500

    Added connection string to connect db and customer form, added entity model for customer by creating a viewmodel

--- CustomerForms/Controllers/HomeController.cs
+++ CustomerForms/Controllers/HomeController.cs
@@ -3,26 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using JunkOutDBModel;
+using CustomerForms.Models;
 
 namespace CustomerForms.Controllers
 {
     public class HomeController : Controller
     {
-        [HttpGet]
+        private JunkoutDBModelContainer db = new JunkoutDBModelContainer();
+
         public ActionResult Index()
         {
             return View();
         }
         [HttpPost]
-        public ActionResult Index(string fname, string bintwo, string binone, string jobtype)
+        public ActionResult Index(CustomerFormModel cfm)
+        // public ActionResult Index(string fname, string bintwo, string binone, string jobtype)
         {
-            return Content($"Hello {fname} {jobtype} {bintwo} {binone}");
+
+             if (ModelState.IsValid)
+              {
+                  db.Customers.Add(cfm.customer);
+                  db.SaveChanges();
+                  return RedirectToAction("About");
+              } 
+
+            // return Content($"Hello {fname} {jobtype} {bintwo} {binone}");
+            return View();
         }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
 
-            return View();
+            return View(db.Addresses.ToList());
         }
 
         public ActionResult Contact()
---
+++ CustomerForms/Models/CustomerFormModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using JunkOutDBModel;
+
+namespace CustomerForms.Models
+{
+    public class CustomerFormModel
+    {
+        public Customer customer { get; set; }
+
+
+    }
+}
