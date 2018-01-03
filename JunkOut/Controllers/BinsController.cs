e761147f1001c48afbdbb55ae62502c2e6777b78
Gursharan Deol <gurdeol02@hotmail.com>
2018-01-03 18:41:16 -0500

    Deleted Reports from Bins

--- JunkOut/Controllers/BinsController.cs
+++ JunkOut/Controllers/BinsController.cs
@@ -179,67 +179,6 @@ namespace JunkOut.Controllers
             base.Dispose(disposing);
         }
 
-        public ActionResult Report(string id)
-        {
-            LocalReport lr = new LocalReport();
-            string path = Path.Combine(Server.MapPath("~/Reports"), "JobType.rdlc");
-            if (System.IO.File.Exists(path))
-            {
-                lr.ReportPath = path;
-            }
-            else
-            {
-                return View("Index");
-            }
-            List<Address> ad = new List<Address>();
-            List<Customer> cust = new List<Customer>();
-            List<Order> ord = new List<Order>();
-
-
-
-            using (JunkoutDBModelContainer jo = new JunkoutDBModelContainer())
-            {
-                ad = jo.Addresses.ToList();
-                cust = jo.Customers.ToList();
-                ord = jo.Orders.ToList();
-            }
-
-
-            ReportDataSource rd = new ReportDataSource("JobType", ord);
-            lr.DataSources.Add(rd);
-            string reportType = id;
-            string mimeType;
-            string encoding;
-            string fileNameExtension;
-
-
-
-            string deviceInfo =
-
-            "<DeviceInfo>" +
-            "  <OutputFormat>" + id + "</OutputFormat>" +
-            "  <PageWidth>8.5in</PageWidth>" +
-            "  <PageHeight>11in</PageHeight>" +
-            "  <MarginTop>0.5in</MarginTop>" +
-            "  <MarginLeft>1in</MarginLeft>" +
-            "  <MarginRight>1in</MarginRight>" +
-            "  <MarginBottom>0.5in</MarginBottom>" +
-            "</DeviceInfo>";
-
-            Warning[] warnings;
-            string[] streams;
-            byte[] renderedBytes;
-
-            renderedBytes = lr.Render(
-                reportType,
-                deviceInfo,
-                out mimeType,
-                out encoding,
-                out fileNameExtension,
-                out streams,
-                out warnings
-                );
-            return File(renderedBytes, mimeType);
-        }
+       
     }
 }
