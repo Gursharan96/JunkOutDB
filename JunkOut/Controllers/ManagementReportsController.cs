1633a5719108fd550e2ef414dc59122fbc6be02b
Gursharan Deol <gurdeol02@hotmail.com>
2018-01-06 09:10:41 -0500

    Added Reports

---
+++ JunkOut/Controllers/ManagementReportsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JunkOut.Controllers
+{
+    public class ManagementReportsController : Controller
+    {
+        // GET: ManagementReports
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
